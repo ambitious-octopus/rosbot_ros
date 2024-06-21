b681cbeca538182728ef6e9cf384837304015d0c
Francesco Mattioli <Francesco.mttl@gmail.com>
2024-06-21 10:25:05 +0200

    changed dockerfile.dev added simulate.sh

--- simulate.sh
+++ simulate.sh
@@ -1,4 +1,4 @@
 source /opt/ros/humble/setup.bash
 source /root/ros_workspace/install/setup.bash
 ros2 run rviz2 rviz2 &
-ros2 launch rosbot_gazebo simulation.launch.py
+ros2 launch rosbot_gazebo simulation.launch.py world:=/ros2_ws/src/husarion_gz_worlds/worlds/empty_with_plugins.sdf
